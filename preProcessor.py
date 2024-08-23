2778fd59acaa96c3d3f8ffda2a90156cd3e57c8c
Atulsahoo333 <atulkumarudb@gmail.com>
2024-08-23 15:25:37 +0530

    Small adjustments
    fixed problem of f string syntax

--- preProcessor.py
+++ preProcessor.py
@@ -13,7 +13,7 @@ def fetch_time_features(df):
     month_dict = {4:1,5:2,6:3,7:4,8:5,9:6,10:7,11:8,12:9,1:10,2:11,3:12}
     df["Financial_Month"] = df["Month"].map(month_dict)
     # Financial year
-    df["Financial_Year"] = df.apply(lambda x: f"{x["Year"]} - {x["Year"]+1}" if x["Month"]>=4 else f"{x["Year"]-1} - {x["Year"]}", axis = 1)
+    df["Financial_Year"] = df.apply(lambda x: f'{x["Year"]} - {x["Year"]+1}' if x["Month"] >= 4 else f'{x["Year"]-1} - {x["Year"]}', axis=1)
     return df
 
 # Multiselect function
